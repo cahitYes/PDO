db8fbfa91ec354ad4021f3ec700dd2e1d8f3fc1e
mikhawa <michaeljpitz@gmail.com>
2022-01-31 14:55:43 +0100

    06 exec CUD

--- pdo_1/06-exec.php
+++ pdo_1/06-exec.php
@@ -5,9 +5,14 @@
 // Nos constantes de configuration
 require_once "01-config.php";
 
+// dépendances pour fakerphp
+require_once '../vendor/autoload.php';
 
 // connexion PDO
 require_once "05-connectPDO.php";
+
+// Instanciation de faker FR
+$faker = Faker\Factory::create('fr_FR');
 
 ?>
 <!DOCTYPE html>
@@ -22,12 +27,14 @@ require_once "05-connectPDO.php";
 
 <body>
     <h3>Exec</h3>
-    <p>$connexion->exec("sql") Pour Creat, update ou delete</p>
+    <p>$connexion->exec("sql") Pour Create, update ou delete</p>
     <h4>INSERT</h4>
     <?php
     try {
+        $login = $faker->word();
+        $name = $faker->name();
         // insertion non unique
-        $nbInsert = $db->exec("INSERT INTO theuser (theuserlogin,theusername) VALUES ('youpie','steve')");
+        $nbInsert = $db->exec("INSERT INTO theuser (theuserlogin,theusername) VALUES ('$login','$name')");
     } catch (Exception $e) {
         // erreurs par défauts
         echo "Erreur ligne {$e->getLine()} du fichier {$e->getFile()} : <br> Code : {$e->getCode()} <br> Message : {$e->getMessage()}<hr>";
@@ -36,8 +43,40 @@ require_once "05-connectPDO.php";
     ?>
     <p><?= $nbInsert ?> entrée insérée</p>
     <?php
+    if ($nbInsert) :
+    ?>
+        <p>Nouvel Article dont l'ID est <?= $db->lastInsertId() ?></p>
+    <?php
+    endif;
+    ?>
+    <h4>UPDATE</h4>
+    <?php
+    try {
+        $nbUpdate = $db->exec("UPDATE thearticle SET theuser_idtheuser=1 WHERE thearticledate LIKE '1971-05%' ");
+    } catch (Exception $e) {
+        // erreurs par défauts
+        echo "Erreur ligne {$e->getLine()} du fichier {$e->getFile()} : <br> Code : {$e->getCode()} <br> Message : {$e->getMessage()}<hr>";
+        die();
+    }
 
 
+    ?>
+    <p><?= $nbUpdate ?> entrée(s) modifiée(s) - (si trouvé mais non modifié, n'est pas renvoyé comme mis à jour !)</p>
+    <h4>DELETE</h4>
+    <?php
+    try {
+        $nbDelete = $db->exec("DELETE FROM thearticle WHERE idthearticle = " . mt_rand(1, 1000) . "");
+    } catch (Exception $e) {
+        // erreurs par défauts
+        echo "Erreur ligne {$e->getLine()} du fichier {$e->getFile()} : <br> Code : {$e->getCode()} <br> Message : {$e->getMessage()}<hr>";
+        die();
+    }
+
+
+    ?>
+    <p><?= $nbDelete ?> entrée(s) supprimée(s)</p>
+
+    <?php
 
     // bonne pratique (sauf si connexion permanante), fermeture de la connexion
     $db = null;
