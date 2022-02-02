5c78401f9c89d5113296a0fffd8d69005b90df10
mikhawa <michaeljpitz@gmail.com>
2022-02-02 18:14:19 +0100

    exe1 view

--- pdo_1/exe1/model/thesectionManager.php
+++ pdo_1/exe1/model/thesectionManager.php
@@ -1,12 +1,16 @@
 <?php
 
 /**
- * Fonction qui sélectionne tous les champs de la table `thesection` ordonnée par `thesectiontitle` ascendant et qui renvoit un tableau vide si pas de résultats
+ * Fonction qui sélectionne tous les champs de la table `thesection` 
+ * ordonnée par `thesectiontitle` ascendant et qui renvoit un tableau vide si pas de résultats,
+ * et un tableau indexé contenant des tableaux associatifs si on a un résultat.
+ * Cette requête ne doit pas être préparée. try catch non nécessaire.
+ * 
  * @param \PDO $con
  * @return array
  */
 function thesectionSelectAll(PDO $con): array
 {
-    // doit renvoyer un tableau (exemple)
+    // doit renvoyer un tableau si pas de résultats
     return [];
 }
---
+++ pdo_1/exe1/view/thesectionHomePage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil de thesection</title>
+</head>
+
+<body>
+    <h1>Accueil de thesection</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>idthesection</th>
+                <th>thesectiontitle</th>
+                <th>thesectiondesc</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
