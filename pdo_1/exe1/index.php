59969f6d9531e1cdb51315def481258d12d0e990
cahitYes <efeyesiloglu@hotmail.com>
2022-02-07 14:59:37 +0100

    index ok

--- pdo_1/exe1/index.php
+++ pdo_1/exe1/index.php
@@ -4,30 +4,62 @@
  * Chargement des dépendances
  */
 require_once "config.php";
-require_once "./model/thesectionManager.php";
-
+require_once "model/thesectionManager.php";
 /**
  * Connexion PDO
  */
 try {
-  
     $db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';port=' . DB_PORT . ';charset=' . DB_CHARSET, DB_LOGIN, DB_PWD);
 
-    
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    
 } catch (Exception $e) {
     echo "Code erreur : " . $e->getCode();
     echo "<br>Message d'erreur : " . $e->getMessage();
-    exit("<h3>Site en maintenance</h3>");
+}
+
+// var_dump($db);
 
 /**
  * Routeur (index.php est l'unique contrôleur)
  */
-}
 
-$allThesection = thesectionSelectAll($db);
-include "view/thesectionhomepage.php";
-    
-    
+/**
+ * Ajout - Crud
+ */
+if (isset($_GET['add'])) {
+
+
+
+    include "view/thesectionAdd.php";
+
+
+    /**
+     * Modification - crUd
+     */
+} elseif (isset($_GET['update']) && ctype_digit($_GET['update'])) {
+
+
+    include "view/thesectionUpdate.php";
+
+
+    /**
+     * Suppression - cruD
+     */
+} elseif (isset($_GET['delete']) && ctype_digit($_GET['delete'])) {
+
+
+    include "view/thesectionDelete.php";
+
+
+    /**
+     * Homepage - cRud
+     */
+} else {
+
+
+    $allThesection = thesectionSelectAll($db);
+
+    // var_dump($allThesection);
+
+    include "view/thesectionHomePage.php";
+}
