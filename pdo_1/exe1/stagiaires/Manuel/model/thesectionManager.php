6b45c8de1594e2906f3f4592603f7f39f29a1c1d
mikhawa <michaeljpitz@gmail.com>
2022-02-07 12:53:30 +0100

    Manuel

---
+++ pdo_1/exe1/stagiaires/Manuel/model/thesectionManager.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Fonction qui sélectionne tous les champs de la table `thesection` 
+ * ordonnée par `thesectiontitle` ascendant et qui renvoie un tableau vide si pas de résultats,
+ * et un tableau indexé contenant des tableaux associatifs si on a un résultat.
+ * Cette requête ne doit pas être préparée. try catch non nécessaire.
+ * 
+ * @param \PDO $con
+ * @return array
+ */
+function thesectionSelectAll(PDO $con): array
+{
+    $sql = "SELECT * FROM thesection ORDER BY thesectiontitle ASC;";
+    $querySection = $db->query($sql);
+    echo $sql['thesection'];
+    // doit renvoyer un tableau si pas de résultats
+    return [];
+}
